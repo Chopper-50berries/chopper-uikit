7a428d99a8b58e9a67d3f46e35371c8a32701f75
Chopper-50berries <chopper.50berries@gmail.com>
2021-06-09 01:45:26 +0800

    init

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -17,7 +17,7 @@ export const brandColors = {
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#FFD1DC",
+  background: "#e4a199",
   backgroundDisabled: "#E9EAEB",
   contrast: "#191326",
   invertedContrast: "#FFFFFF",
@@ -37,7 +37,7 @@ export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
   secondary: "#136B49",
-  background: "#343135",
+  background: "#e4a199",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
   invertedContrast: "#191326",
