d908d6a6812c2f9fa6072a7228da1299c4c085ba
Chopper-50berries <chopper.50berries@gmail.com>
2021-06-10 18:32:17 +0800

    init

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2721,20 +2721,20 @@ var templateObject_1;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#136B49",
-    primaryBright: "#136B49",
-    primaryDark: "#136B49",
-    secondary: "#136B49",
+    primary: "#da2c43",
+    primaryBright: "#da2c43",
+    primaryDark: "#da2c43",
+    secondary: "#da2c43",
     success: "#31D0AA",
     warning: "#FFB237",
 };
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#e4a199", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#403535", textDisabled: "#BDC2C4", textSubtle: "#403535", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#da2c43", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#403535", textDisabled: "#BDC2C4", textSubtle: "#403535", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#136B49", background: "#e4a199", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#136B49", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#136B49", background: "#da2c43", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#136B49", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,10 +2,10 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#136B49",
-  primaryBright: "#136B49",
-  primaryDark: "#136B49",
-  secondary: "#136B49",
+  primary: "#da2c43",
+  primaryBright: "#da2c43",
+  primaryDark: "#da2c43",
+  secondary: "#da2c43",
   success: "#31D0AA",
   warning: "#FFB237",
 };
@@ -17,7 +17,7 @@ export const brandColors = {
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#e4a199",
+  background: "#da2c43",
   backgroundDisabled: "#E9EAEB",
   contrast: "#191326",
   invertedContrast: "#FFFFFF",
@@ -37,7 +37,7 @@ export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
   secondary: "#136B49",
-  background: "#e4a199",
+  background: "#da2c43",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
   invertedContrast: "#191326",
