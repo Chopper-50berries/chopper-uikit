2bfbdf3951aeeb64c92f6bc5ec9aa3a4099e54af
Chopper-50berries <chopper.50berries@gmail.com>
2021-06-12 04:54:23 +0800

    init

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2709,20 +2709,20 @@ var templateObject_1;
 var baseColors = {
     failure: "#ED4B9E",
     primary: "#00FF00",
-    primaryBright: "#00FF00",
+    primaryBright: "#d2ed4b",
     primaryDark: "#da2c43",
-    secondary: "#da2c43",
+    secondary: "#4bede2",
     success: "#31D0AA",
-    warning: "#FFB237",
+    warning: "#FFB237", // orange unseen
 };
 var brandColors = {
-    binance: "#F0B90B",
+    binance: "#F0B90B", // unseen gold
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#e4a199", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#2da8d2", textDisabled: "#BDC2C4", textSubtle: "#2da8d2", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
-        bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#e4a199", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#b55312", text: "#2da8d2", textDisabled: "#BDC2C4", textSubtle: "#e84bed", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
+        bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)", // unused background banner from pancake Farms // light blue & light purple not seen
     } });
 var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#136B49", background: "#da2c43", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#136B49", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
-        bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+        bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)", // navy blue & dark purple
     } });
 
 var light$6 = {
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,35 +1,35 @@
 import { Colors } from "./types";
 
 export const baseColors = {
-  failure: "#ED4B9E",
-  primary: "#00FF00",
-  primaryBright: "#00FF00",
-  primaryDark: "#da2c43",
-  secondary: "#da2c43",
-  success: "#31D0AA",
-  warning: "#FFB237",
+  failure: "#ED4B9E", // pink unseen
+  primary: "#00FF00", // bright green
+  primaryBright: "#d2ed4b", // lime green // testing
+  primaryDark: "#da2c43", // red
+  secondary: "#4bede2", // bright turquiuose // pressed button colour (?)
+  success: "#31D0AA", // toggle button on green
+  warning: "#FFB237", // orange unseen
 };
 
 export const brandColors = {
-  binance: "#F0B90B",
+  binance: "#F0B90B", // unseen gold
 };
 
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#e4a199",
-  backgroundDisabled: "#E9EAEB",
-  contrast: "#191326",
-  invertedContrast: "#FFFFFF",
-  input: "#eeeaf4",
-  tertiary: "#EFF4F5",
-  text: "#2da8d2",
-  textDisabled: "#BDC2C4",
-  textSubtle: "#2da8d2",
-  borderColor: "#E9EAEB",
-  card: "#FFFFFF",
+  background: "#e4a199", // pink 
+  backgroundDisabled: "#E9EAEB", // light grey // bg of disabled Harvest button
+  contrast: "#191326", // unused from pancake Lottery // dark purple
+  invertedContrast: "#FFFFFF", // unused from pancake Lottery // white
+  input: "#eeeaf4", // faint purple // bg of widgets i.e. Staked Only & Active / Inactive in Farms
+  tertiary: "#b55312", // brown // testing
+  text: "#2da8d2", // chopper blue
+  textDisabled: "#BDC2C4", // medium grey // grey-ed out text
+  textSubtle: "#e84bed", // bright purple // testing
+  borderColor: "#E9EAEB", // light grey // divider line in cards
+  card: "#FFFFFF", // white
   gradients: {
-    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)", // unused background banner from pancake Farms // light blue & light purple not seen
   },
 };
 
@@ -50,6 +50,6 @@ export const darkColors: Colors = {
   borderColor: "#524B63",
   card: "#27262c",
   gradients: {
-    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)", // navy blue & dark purple
   },
 };
